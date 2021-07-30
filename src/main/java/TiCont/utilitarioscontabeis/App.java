41722f371aa1253b8c612994cc57d518ef251864
ricardomendes95 <sr_ricardomendes@hotmail.com>
2021-07-30 15:39:58 -0300

    initial project maven

---
+++ src/main/java/TiCont/utilitarioscontabeis/App.java
@@ -0,0 +1,13 @@
+package TiCont.utilitarioscontabeis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
